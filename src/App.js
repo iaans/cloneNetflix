a649dcecc17889d40928d0383ba9ab1d5d4d0c1f
Ian Augusto Silva <ian.silva@venturus.org.br>
2021-09-09 18:21:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,20 +2,36 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Tmdb from "./Tmdb.js";
 import MovieRow from "./components/MovieRow.js";
+import FeaturedMovie from "./components/FeaturedMovie";
 
 export default () => {
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(() => {
     const loadAll = async () => {
+      //pegando a lista TOTAL
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      //Pegando o Featured
+      let originals = list.filter((i) => i.slug === "originals");
+
+      if (originals && originals.length > 0) {
+        let randomChosen = Math.floor(
+          Math.random() * (originals[0].items.results.length - 1)
+        );
+        let chosen = originals[0].items.results[randomChosen];
+        let chosenInfo = await Tmdb.getMovieInfo(chosen.id, "tv");
+        setFeaturedData(chosenInfo);
+      }
     };
     loadAll();
   }, []);
 
   return (
     <div className="page">
+      {featuredData && <FeaturedMovie item={featuredData} />}
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
--- src/Tmdb.js
+++ src/Tmdb.js
@@ -22,7 +22,7 @@ export default {
   getHomeList: async () => {
     return [
       {
-        slug: "originais",
+        slug: "originals",
         title: "Originais do Netflix",
         items: await basicFetch(
           `/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`
@@ -30,7 +30,7 @@ export default {
       },
       {
         slug: "treding",
-        title: "Recomendados para Você",
+        title: "Recomendos para Você",
         items: await basicFetch(
           `/trending/all/week?language=pt-BR&api_key=${API_KEY}`
         ),
@@ -39,44 +39,68 @@ export default {
         slug: "toprated",
         title: "Em Alta",
         items: await basicFetch(
-          `/movie/top/top_rated?language=pt-BR&api_key=${API_KEY}`
+          `/movie/top_rated?language=pt-BR&api_key=${API_KEY}`
         ),
       },
       {
         slug: "action",
         title: "Ação",
         items: await basicFetch(
-          `/discover/movie?with_genres=28&language=pt-BR&api_key${API_KEY}`
+          `/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`
         ),
       },
       {
         slug: "comedy",
         title: "Comédia",
         items: await basicFetch(
-          `/discover/movie?with_genres=35&language=pt-BR&api_key${API_KEY}`
+          `/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`
         ),
       },
       {
         slug: "terror",
         title: "Terror",
         items: await basicFetch(
-          `/discover/movie?with_genres=27&language=pt-BR&api_key${API_KEY}`
+          `/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`
         ),
       },
       {
         slug: "romance",
         title: "Romance",
         items: await basicFetch(
-          `/discover/movie?with_genres=10749&language=pt-BR&api_key${API_KEY}`
+          `/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`
         ),
       },
       {
         slug: "documentary",
         title: "Documentários",
         items: await basicFetch(
-          `/discover/movie?with_genres=99&language=pt-BR&api_key${API_KEY}`
+          `/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`
         ),
       },
     ];
+  },
+
+  getMovieInfo: async (movieId, type) => {
+    let info = {};
+
+    if (movieId) {
+      switch (type) {
+        case "movie":
+          info = await basicFetch(
+            `/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`
+          );
+          break;
+        case "tv":
+          info = await basicFetch(
+            `/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`
+          );
+          break;
+        default:
+          info = null;
+          break;
+      }
+    }
+
+    return info;
   },
 };
